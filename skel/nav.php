269b281e46fed45c26bd2e2f27fba16920441ac1
Loay Samha <lsamha@csub.edu>
2020-12-04 07:07:56 -0800

    Created a home button

--- skel/nav.php
+++ skel/nav.php
@@ -1,11 +1,21 @@
 </head>
 <body>
  <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-        <div class="container"><a class="navbar-brand logo" href="#">Bshop</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse"
-                id="navcol-1">
+        <div class="container">
+          <a class="navbar-brand logo" href="./adminDash.php">Bshop</a>
+
+          <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span>
+          </button>
+
+            <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="../../backend/includes/logout.inc.php">logout</a></li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../../backend/includes/logout.inc.php">logout</a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="./adminDash.php">HOME<span class="sr-only">(current)</span></a>
+                    </li>
+
                 </ul>
             </div>
         </div>
