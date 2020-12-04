ab49ea33b21e99626d1a5e7b2204012052b8eb49
Loay Samha <lsamha@csub.edu>
2020-12-04 06:42:48 -0800

    Added register button

--- adminDash.php
+++ adminDash.php
@@ -7,30 +7,35 @@ session_start();
 #} else {
 
     echo '<title>Admin Dashboard</title>';
-    
+
     include('./skel/header.php');
     include('./skel/nav.php');
 #====================================BODY=======================================
 ?>
 <main class="page">
-<section class="clean-block dark">
-<div class="container">
-<br>
-<h1> Good to be back right (NAME) </h1>
-<div class="input-group">
-  <select class="custom-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-    <option selected>Choose...</option>
-    <option value="1">Today</option>
-    <option value="2">This week</option>
-    <option value="3">This month</option>
-    <option value="4">This year</option>
-  </select>
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="button">Average Sale</button>
-  </div>
-</div>
-</div>
-</section>
+  <section class="clean-block dark">
+      <div class="container">
+        <br>
+        <h1> Good to be back right (NAME) </h1>
+        <div class="input-group">
+          <select class="custom-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+            <option selected>Choose...</option>
+            <option value="1">Today</option>
+            <option value="2">This week</option>
+            <option value="3">This month</option>
+            <option value="4">This year</option>
+          </select>
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button">Average Sale</button>
+        </div>
+      </div>
+      <br>
+      <div class="Register">
+      <button type="button" class="btn btn-outline-warning">Register New User</button>
+      </div>
+      <br>
+    </div>
+  </section>
 </main>
 <?php
 #=================================END-BODY======================================
