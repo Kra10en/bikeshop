8e0840f586ed9c431675429e3ce9f7ac7f33a07e
Randeep Mangat <mangattrandy017@gmail.com>
2020-12-14 17:34:54 -0800

    Modified the register_emp file, will now also take email as a parameter

--- db/register_emp.sql
+++ db/register_emp.sql
@@ -1,10 +1,10 @@
 DELIMITER //
 DROP PROCEDURE add_employee //
 CREATE or REPLACE PROCEDURE add_employee
-(param_fname varchar(55), param_lname varchar(55), param_password text)
+(param_fname varchar(55), param_lname varchar(55), param_email varchar(255), param_password text)
 BEGIN
 	DECLARE param_personid int;
-	INSERT INTO person (`f-name`, `l-name`) VALUES(param_fname, param_lname);
+	INSERT INTO person (`f-name`, `l-name`, `e-mail`) VALUES(param_fname, param_lname, param_email);
 	SELECT MAX(`person-id`) INTO param_personid FROM person;
 	INSERT INTO employee(`person-id`, password) VALUES(param_personid, param_password);
 END;
