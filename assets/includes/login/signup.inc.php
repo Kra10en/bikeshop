a2590a61bb699648d320b6803ad6cd726eb79645
Loay Samha <lsamha@csub.edu>
2020-12-15 12:23:10 -0800

    changing boolean values for if statments conditions

--- assets/includes/login/signup.inc.php
+++ assets/includes/login/signup.inc.php
@@ -17,7 +17,7 @@ if (isset($_POST["addEmpSubmit"])) {
 #    header("location: ../../../signup.php?error=emptyfields");
 #    exit();
 #  }
-  if (wrongEmail($empEmail) == false ) {
+  if (wrongEmail($empEmail) == true ) {
     header("location: ../../../signup.php?error=wrongemail");
     exit();
   }
@@ -25,7 +25,7 @@ if (isset($_POST["addEmpSubmit"])) {
     header("location: ../../../signup.php?error=nomatchpasswords");
     exit();
   }
-  if (emailExists($conn, $empEmail) == true ) {
+  if (emailExists($conn, $empEmail) == false ) {
     header("location: ../../../signup.php?error=emailexists");
     exit();
   }
