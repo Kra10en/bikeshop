112221988ea34021b5a7cf769290f7fd02a5a4b7
Loay Samha <lsamha@csub.edu>
2020-12-14 17:24:41 -0800

    Created new fields and edited the title

--- signup.php
+++ signup.php
@@ -17,13 +17,16 @@ session_start();
         <section class="clean-block clean-form dark">
             <div class="container">
                 <div class="block-heading">
-                    <h2 class="text-info">Registration</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                    <h2 class="text-info">Registrationi</h2>
+                    <p>This page is to register an employee only accessable by the admin.</p>
                 </div>
                 <form>
-                    <div class="form-group"><label for="name">Name</label><input class="form-control item" type="text" id="name"></div>
-                    <div class="form-group"><label for="password">Password</label><input class="form-control item" type="password" id="password"></div>
-                    <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" id="email"></div><button class="btn btn-primary btn-block" type="submit">Sign Up</button></form>
+                    <div class="form-group"><label for="name">First Name</label><input class="form-control item" name="fname" type="text" id="name"></div>
+                    <div class="form-group"><label for="name">Last Name</label><input class="form-control item" name="lname" type="text" id="name"></div>
+                    <div class="form-group"><label for="email">Email</label><input class="form-control item" name="email" type="email" id="email"></div>
+                    <div class="form-group"><label for="password">Password</label><input class="form-control item" type="password" placeholder="*****************" id="password"></div>
+                    <div class="form-group"><label for="password">Confirm Password</label><input class="form-control item" type="password" placeholder="*****************" id="password"></div>
+                    <button class="btn btn-primary btn-block" type="submit">Sign Up</button></form>
             </div>
         </section>
     </main>
