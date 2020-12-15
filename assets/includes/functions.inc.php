4f6944c059f8b5859d860261ea01acce67d74b72
Loay Samha <lsamha@csub.edu>
2020-12-15 11:24:03 -0800

    Created 3 functions getEmpID() checkEmpID() loginEmp(). All 3 functions depend on each other. I hope its working because its kinda complicated to gather all around and just to link to tables to each other and then verify the password. Took more than 1 function to finish that. Though, I have no idea its working or not LOL

--- assets/includes/functions.inc.php
+++ assets/includes/functions.inc.php
@@ -87,21 +87,86 @@ function emptyFieldLogin($empEmail, $empPwd) {
   return $result;
 }
 
+#function to get employee-id
+function getEmpId($conn, $empEmail) {
+  $emailExists = emailExists($conn, $empEmail);
+  if ($emailExists === false) {
+    header("location: ../../../login.php?error=invalidloginemail");
+    exit();
+  }
+  $empID = "SELECT `person-id` FROM person WHERE `e-mail`=?; ";
+  $stmt = mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt, $empID)) {
+    header("location: ../../../signup.php?error=sqlfail");
+    exit();
+  }
+
+  mysqli_stmt_bind_param($stmt, "ss", $empEmail);
+  mysqli_stmt_execute($stmt);
+
+  $resultStmt = mysqli_stmt_get_result($stmt);
+
+  if ($row = mysqli_fetch_assoc($resultStmt)){
+    return $row;
+  } else {
+    $result = false;
+    return $result;
+  }
+
+  mysqli_stmt_close($stmt);
+
+}
+
+#function to check password based on getEmpId()
+function checkEmpID($conn, $empEmail) {
+  $empIDExists = getEmpId($conn, $empEmail);
+  if ($empIDExists === false) {
+    header("location: ../../../login.php?error=invalidlogin");
+    exit();
+  }
+  $emp = "SELECT * FROM employee WHERE `person-id`=?; ";
+  $stmt = mysqli_stmt_init($conn);
+  if(!mysqli_stmt_prepare($stmt, $emp)) {
+    header("location: ../../../signup.php?error=sqlfail");
+    exit();
+  }
+
+  mysqli_stmt_bind_param($stmt, "ss", $empIDExists);
+  mysqli_stmt_execute($stmt);
+
+  $resultStmt = mysqli_stmt_get_result($stmt);
+
+  if ($row = mysqli_fetch_assoc($resultStmt)){
+    return $row;
+  } else {
+    $result = false;
+    return $result;
+  }
+
+  mysqli_stmt_close($stmt);
+}
+
 #function created to log the user in.
 function loginEmp($conn, $empEmail, $empPwd) {
+  $checkEmp = checkEmpID($conn, $empEmail);
   $emailExists = emailExists($conn, $empEmail);
 
+  if ($checkEmp === false) {
+    header("location: ../../../login.php?error=invalidlogin");
+    exit();
+  }
   if ($emailExists === false) {
     header("location: ../../../login.php?error=invalidlogin");
     exit();
   }
-  $empPwdHashed = $emailExists["password"];
+
+  $empPwdHashed = $checkEmp["password"];
 
   $checkPwd = password_verify($empPwd, $empPwdHashed);
 
   if ($checkPwd === true) {
     session_start();
-    $_SESSION["userID"] = $emailExists["employee-id"];
+    $_SESSION["userID"] = $checkEmp["employee-id"];
     $_SESSION["userE"] = $emailExists["e-mail"];
     header("location: ../../../employeeDash.php");
     exit();
