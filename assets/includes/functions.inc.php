bf26131f2a22ac8782e7ea67557b68b9e2566862
Loay Samha <lsamha@csub.edu>
2020-12-15 12:46:19 -0800

    added else statement to mysqliPrepareStatment in emailExists()

--- assets/includes/functions.inc.php
+++ assets/includes/functions.inc.php
@@ -41,20 +41,20 @@ function emailExists($conn, $empEmail) {
   if(!mysqli_stmt_prepare($stmt, $sql)) {
     header("location: ../../../signup.php?error=sqlfail");
     exit();
-  }
-
-  mysqli_stmt_bind_param($stmt, "s", $empEmail);
-  mysqli_stmt_execute($stmt);
-
-  $resultStmt = mysqli_stmt_get_result($stmt);
-  
-  if ($row = mysqli_fetch_assoc($resultStmt)){
-    return $row;
   } else {
-    $result = false;
-    return $result;
-  }
 
+    mysqli_stmt_bind_param($stmt, "s", $empEmail);
+    mysqli_stmt_execute($stmt);
+
+    $resultStmt = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultStmt)){
+      return $row;
+    } else {
+      $result = false;
+      return $result;
+    }
+  }
   mysqli_stmt_close($stmt);
 }
 
