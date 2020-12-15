8d942d4c90461d2521ea1ea96e7eb67596a78250
Loay Samha <lsamha@csub.edu>
2020-12-14 16:02:14 -0800

    Created a wrongUID function to limit employee username validatiy for only a-zA-Z0-9 only

--- assets/includes/functions.inc.php
+++ assets/includes/functions.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+# function used in the signup.inc.php used to check if fields are empty
 function emptyFieldSignup($empName, $empEmail, $empUID, $empPwd, $empPwdConfirm) {
   $result;
   if (empty($empName) || empty($emptyEmail) || empty($empUID) || empty($empPwd) || empty($empPwdConfirm)) {
@@ -7,4 +8,16 @@ function emptyFieldSignup($empName, $empEmail, $empUID, $empPwd, $empPwdConfirm)
   } else {
     $result = false;
   }
+  return $result;
+}
+
+#function used in the signup.inc.php used to check if the user is properly formatted
+function wrongUID($empUID) {
+  $result;
+  if (preg_match("/^[a-zA-Z0-9]*$/"), $empUID) {
+    $result = true;
+  } else {
+    $result = false;
+  }
+  return $result;
 }
