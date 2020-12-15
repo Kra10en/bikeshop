6df832d8213900c4f390bf96ad3baf1bcbc5b79a
Randeep Mangat <mangattrandy017@gmail.com>
2020-12-15 11:52:55 -0800

    Creadte sql file for query to show avg sales over last week

---
+++ db/saleavg_week.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+DROP PROCEDURE avgBikeSale //
+CREATE PROCEDURE avgBikeSale()
+BEGIN 
+	DECLARE `current_time` datetime;
+	SELECT SUBDATE(CURRENT_DATE, INTERVAL 7 DAY) INTO `current_time`;
+SELECT AVG(`pulls-total`) FROM pulls
+INNER JOIN sale ON sale.`pulls-id` = pulls.`pulls-id`
+WHERE sale.`sale-datetime` < current_time;
+END;
+//
+DELIMITER ;
+
